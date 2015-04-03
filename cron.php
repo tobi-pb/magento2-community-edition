50763eaa0ac1e98aea5b3d1a637d54da575c6efd
Arkadii Chyzhov <achyzhov@ebay.com>
2015-04-03 16:24:20 +0300

    MAGETWO-35306: Implement Main Flow in Cron Script

--- cron.php
+++ cron.php
@@ -6,7 +6,32 @@
 
 require_once __DIR__ . '/app/bootstrap.php';
 
-$updateStatusFile = fopen(__DIR__ . '/var/.update_status.txt', 'w');
-$demoObject = new Magento\DemoClass();
-fwrite($updateStatusFile, $demoObject->getCurrentDateTime());
-fclose($updateStatusFile);
+$updateInProgressFlagFilename = UPDATER_BP . '/var/.update_in_progress.flag';
+
+if (file_exists($updateInProgressFlagFilename)) {
+    exit('Cron is already in progress...');
+}
+
+$updateInProgressFlagFile = fopen($updateInProgressFlagFilename, 'w');
+if (!$updateInProgressFlagFile) {
+    exit('Cron could not open ' . $updateInProgressFlagFilename);
+}
+
+/** @var \Magento\Update\Queue $jobQueue */
+$jobQueue = new \Magento\Update\Queue();
+
+/** @var \Magento\Update\Status $jobLog */
+$jobLog = new \Magento\Update\Status();
+
+/** @var \Magento\Update\Queue\AbstractJob $job*/
+foreach ($jobQueue->popQueuedJobs() as $job) {
+    try {
+        $job->execute();
+    } catch (Exception $e) {
+        $jobLog->add(sprintf('An error occurred while executing job %s: %s', $job->getName(), $e->getMessage()));
+    }
+}
+fclose($updateInProgressFlagFile);
+if (file_exists($updateInProgressFlagFilename)) {
+    unlink($updateInProgressFlagFilename);
+}
