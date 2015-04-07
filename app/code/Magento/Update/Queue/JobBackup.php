c1798a8876367261bda8099c391eb598431704c0
Arkadii Chyzhov <achyzhov@ebay.com>
2015-04-07 14:21:59 +0300

    MAGETWO-35312: Implement Script for Backup Job Processing
    - added mock object to test

--- app/code/Magento/Update/Queue/JobBackup.php
+++ app/code/Magento/Update/Queue/JobBackup.php
@@ -17,13 +17,29 @@ class JobBackup extends AbstractJob
 {
     // TODO: Add job specific getters and there initialization based on construct params in scope of https://jira.corp.x.com/browse/MAGETWO-35312
 
+    /** @var \Magento\Update\Backup\BackupInfo */
+    protected $backupInfo;
+
+    /**
+     * Initialize job instance.
+     *
+     * @param string $name
+     * @param object $params
+     * @param \Magento\Update\Status $jobStatus
+     * @param \Magento\Update\Backup\BackupInfo $backupInfo
+     */
+    public function __construct($name, $params, $jobStatus = null, $backupInfo = null)
+    {
+        parent::__construct($name, $params, $jobStatus);
+        $this->backupInfo = $backupInfo ? $backupInfo : new BackupInfo();
+    }
+
     /**
      * {@inheritdoc}
      */
     public function execute()
     {
-        $backupInfo = new BackupInfo();
-        $archivator = $this->createArchivator($backupInfo);
+        $archivator = $this->createArchivator($this->backupInfo);
         $this->jobStatus->add(sprintf('%s: Backup %s has been created', $this->getName(), $archivator->archive()));
         return $this;
     }
--- dev/tests/integration/testsuite/Magento/Update/Queue/JobBackupTest.php
+++ dev/tests/integration/testsuite/Magento/Update/Queue/JobBackupTest.php
@@ -8,6 +8,9 @@ namespace Magento\Update\Queue;
 class JobBackupTest extends \PHPUnit_Framework_TestCase
 {
     /** @var  string */
+    protected $backupFilename;
+
+    /** @var  string */
     protected $backupPath;
 
     /** @var array */
@@ -16,29 +19,45 @@ class JobBackupTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         parent::setUp();
+        $this->backupFilename = uniqid('test_backup') . '.zip';
         $this->backupPath = UPDATER_BP . '/var/backup/';
     }
 
     public function testArchive()
     {
-        $this->markTestSkipped('MAGETWO-35283');
         $jobName = 'Backup';
         $jobStatus = new \Magento\Update\Status();
         $jobStatus->clear();
-        $jobBackup = new \Magento\Update\Queue\JobBackup($jobName, [], $jobStatus);
+
+        $backupInfo = $this->getMockBuilder('Magento\Update\Backup\BackupInfo')
+            ->disableOriginalConstructor()
+            ->setMethods(['getBackupFilename','getBlacklist', 'getArchivedDirectory'])
+            ->getMock();
+        $backupInfo->expects($this->any())
+            ->method('getBackupFilename')
+            ->willReturn($this->backupFilename);
+        $backupInfo->expects($this->any())
+            ->method('getArchivedDirectory')
+            ->willReturn(UPDATER_BP);
+        $backupInfo->expects($this->any())
+            ->method('getBlacklist')
+            ->willReturn(['/var/backup', '/vendor', '/app/code']);
+
+        $jobBackup = new \Magento\Update\Queue\JobBackup($jobName, [], $jobStatus, $backupInfo);
         $this->dirList = scandir($this->backupPath);
 
         $jobBackup->execute();
 
         $tmpFiles = array_diff(scandir($this->backupPath), $this->dirList);
-        $backupFile = array_pop($tmpFiles);
+        $actualBackupFile = array_pop($tmpFiles);
+        $this->assertEquals($this->backupFilename, $actualBackupFile);
 
         $actualJobStatus = $jobStatus->get();
-        $expectedJobStatus = $jobName . ': Backup ' . $backupFile . ' has been created';
-        $this->assertEquals($expectedJobStatus, $actualJobStatus);
+        $expectedJobStatus = $jobName . ': Backup ' . $actualBackupFile . ' has been created';
+        $this->assertRegExp('/' . $expectedJobStatus . '/', $actualJobStatus);
 
-        if (file_exists($this->backupPath . $backupFile)) {
-            unlink($this->backupPath . $backupFile);
+        if (file_exists($this->backupPath . $actualBackupFile)) {
+            unlink($this->backupPath . $actualBackupFile);
         }
     }
 }
